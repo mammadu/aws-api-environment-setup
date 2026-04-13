c39882688173ff964e207a72d53e7f6709f6c6af
mammadu <mammadudiallo@gmail.com>
2026-04-13 14:05:53 -0700

    update install-ansible.sh to run without prompting yes/no

--- install-ansible.sh
+++ install-ansible.sh
@@ -1,1 +1,1 @@
-sudo apt update && sudo apt install software-properties-common && sudo add-apt-repository --yes --update ppa:ansible/ansible && sudo apt install ansible
+sudo apt update && sudo apt install software-properties-common --yes && sudo add-apt-repository --yes --update ppa:ansible/ansible && sudo apt install ansible --yes
